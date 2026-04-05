e2dd04e7896c74339e2513cf9e473ea7534c9ff8
Venetia Furtado <venetiarosita@gmail.com>
2026-04-05 02:59:09 +0000

    added i2c support

--- bme280_sensor/bme280_sensor.c
+++ bme280_sensor/bme280_sensor.c
@@ -14,11 +14,17 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/printk.h>
+#include <linux/miscdevice.h>
+#include <linux/i2c.h>
 #include <linux/types.h>
 #include <linux/cdev.h>
 #include <linux/slab.h> // required for kmalloc
 #include <linux/fs.h>   // file_operations
 #include "bme280_sensor.h"
+
+#define BME280_SENSOR_DEVICE "bme280_sensor"
+#define BME280_CHIP_ID_REGISTER 0xD0
+#define BME280_CHIP_ID_VALUE 0x60
 
 int sensor_major = 0; // use dynamic major
 int sensor_minor = 0;
@@ -79,13 +85,7 @@ ssize_t sensor_read(struct file *filp, char __user *buf, size_t count,
     return retval;
 }
 
-struct file_operations sensor_fops = {
-    .owner = THIS_MODULE,
-    .read = sensor_read,
-    .open = sensor_open,
-    .release = sensor_release,
-};
-
+#if 0
 static int sensor_setup_cdev(struct sensor_dev *dev)
 {
     int err, devno = MKDEV(sensor_major, sensor_minor);
@@ -133,6 +133,82 @@ void sensor_cleanup_module(void)
 
     unregister_chrdev_region(devno, 1);
 }
+#endif
 
-module_init(sensor_init_module);
-module_exit(sensor_cleanup_module);
+struct file_operations bme280_sensor_fops = {
+    .owner = THIS_MODULE,
+    .read = sensor_read,
+    .open = sensor_open,
+    .release = sensor_release,
+};
+
+static struct miscdevice bme280_sensor_device = {
+    .minor = MISC_DYNAMIC_MINOR,
+    .name = BME280_SENSOR_DEVICE,
+    .fops = &bme280_sensor_fops,
+};
+
+static int bme280_sensor_probe(struct i2c_client *client,
+                               const struct i2c_device_id *id)
+{
+    int status;
+    uint8_t who;
+
+    who = i2c_smbus_read_byte_data(client, BME280_CHIP_ID_REGISTER);
+    if (who != BME280_CHIP_ID_VALUE)
+    {
+        dev_err(&client->dev,
+                "unknown CHIP ID: 0x%02x\n", who);
+        return -ENODEV;
+    }
+
+    PDEBUG("DEBUG: bme280 sensor found");
+
+    /* register device */
+    status = misc_register(&bme280_sensor_device);
+    if (status != 0)
+    {
+        dev_err(&client->dev,
+                "misc_register failed: %d\n", status);
+        return status;
+    }
+
+    PDEBUG("DEBUG: bme280 sensor registered");
+
+    dev_info(&client->dev, "bme280 sensor driver loaded\n");
+    return 0;
+
+    /*
+    TODO: deregister if register and we have error
+err_misc:
+    misc_deregister(&bme280_sensor_device);
+    return ret;
+    */
+}
+
+static int bme280_sensor_remove(struct i2c_client *client)
+{
+    misc_deregister(&bme280_sensor_device);
+    PDEBUG("DEBUG: bme280 sensor removed");
+    dev_info(&client->dev, "bme280 sensor unloaded\n");
+    return 0;
+}
+
+static const struct i2c_device_id bme280_sensor_id[] = {
+    {BME280_SENSOR_DEVICE, 0},
+    {}};
+MODULE_DEVICE_TABLE(i2c, bme280_sensor_id);
+
+static struct i2c_driver bme280_sensor_driver = {
+    .driver = {
+        .name = BME280_SENSOR_DEVICE,
+    },
+    .probe = bme280_sensor_probe,
+    .remove = bme280_sensor_remove,
+    .id_table = bme280_sensor_id,
+};
+
+module_i2c_driver(bme280_sensor_driver);
+
+// module_init(sensor_init_module);
+// module_exit(sensor_cleanup_module);
